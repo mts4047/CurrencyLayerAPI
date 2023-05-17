4791ff046a8a43aba4d823c16a59136e8308fb57
MTS <40478665@live.napier.ac.uk>
2023-05-18 03:16:44 +0630

    Initial Commit

---
+++ CurrencyLayerAPI/CurrencyLayerAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyLayerAPIApp.swift
+//  CurrencyLayerAPI
+//
+//  Created by Myo Thandar soe on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyLayerAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
